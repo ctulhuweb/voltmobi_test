25275140c88271d695eab9323c50fdd40c7ba365
Pyatov Nikita <ctulhuweb@gmail.com>
2020-09-25 17:43:05 +0700

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+TEAMS_COUNT = 2
+INDICATORS_COUNT = 2
+PLAYER_INDICATORS = 30
+
+teams = FactoryBot.create_list(:team, TEAMS_COUNT, :with_players, :with_match)
+indicators = FactoryBot.create_list(:indicator, INDICATORS_COUNT)
+
+players = Player.all
+
+PLAYER_INDICATORS.times do
+  pl = players[rand(players.size - 1)]
+  player_matches = pl.matches
+  match = player_matches[rand(player_matches.size - 1)] 
+  FactoryBot.create(:player_indicator, player: pl, match: match, indicator: indicators.shuffle.first) 
+end
+
--- spec/factories/team.rb
+++ spec/factories/team.rb
@@ -2,4 +2,20 @@ FactoryBot.define do
   factory :team do
     name { Faker::Sports::Football.team }
   end
+
+  trait :with_players do
+    transient do 
+      players_count { 5 }
+    end
+
+    after(:create) do |team, evaluator|
+      FactoryBot.create_list(:player, evaluator.players_count, team: team)
+    end
+  end
+
+  trait :with_match do
+    after(:create) do |team|
+      FactoryBot.create(:match, team: team)
+    end
+  end
 end
